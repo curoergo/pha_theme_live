35f5f1f7ffaae2679740abdfbcec80dfb5a93875
Jamie Malcolm-Brown <jamie@curoergosum.com>
2016-04-13 17:22:07 -0400

    Add in the nav for slider

--- js/pha_theme2.js
+++ js/pha_theme2.js
@@ -1,3 +1,8 @@
 jQuery(document).ready(function ($) {
-    $(".rslides").responsiveSlides();
+    $(".rslides").responsiveSlides({
+        auto: true, // Boolean: Animate automatically, true or false
+        speed: 500, // Integer: Speed of the transition, in milliseconds
+        timeout: 4000, // Integer: Time between slide transitions, in milliseconds
+        nav: true, // Boolean: Show navigation, true or false
+    });
 });
