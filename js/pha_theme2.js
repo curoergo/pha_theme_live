02f65d76a7c4c63a8fc02e7501dc45a99fee8c87
Jamie Malcolm-Brown <jamie@curoergosum.com>
2016-04-14 12:46:35 -0400

    Test slider changes

--- js/pha_theme2.js
+++ js/pha_theme2.js
@@ -1,8 +1,7 @@
 jQuery(document).ready(function ($) {
-    $(".rslides").responsiveSlides({
+    $(".page-node-686016 .rslides").responsiveSlides({
         auto: true, // Boolean: Animate automatically, true or false
-        speed: 500, // Integer: Speed of the transition, in milliseconds
+        speed: 700, // Integer: Speed of the transition, in milliseconds
         timeout: 4000, // Integer: Time between slide transitions, in milliseconds
-        nav: true, // Boolean: Show navigation, true or false
     });
 });
