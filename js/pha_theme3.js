e567a4e4ca22d37a573e317382f6cb7e3e282425
Jamie Malcolm-Brown <jamie@curoergosum.com>
2016-04-14 22:27:55 -0400

    Slideshow updates

--- js/pha_theme3.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function ($) {
-    $(".page-node-686016 .rslides").responsiveSlides({
-        auto: true, // Boolean: Animate automatically, true or false
-        speed: 700, // Integer: Speed of the transition, in milliseconds
-        timeout: 4000, // Integer: Time between slide transitions, in milliseconds
-    });
-});
---
+++ js/pha_theme4.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function ($) {
+    $(".page-node-686016 .rslides").responsiveSlides({
+        auto: true,
+        speed: 700,
+        timeout: 4000,
+        pager: false,
+        nav: true
+    });
+});
