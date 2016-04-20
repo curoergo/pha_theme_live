6f85559d1bc494212b656a1cb7ed8ea4e590f014
Jamie Malcolm-Brown <jamie@curoergosum.com>
2016-04-20 12:50:34 -0400

    Slider updates

--- js/pha_theme4.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function ($) {
-    $(".page-node-686016 .rslides").responsiveSlides({
-        auto: true,
-        speed: 700,
-        timeout: 4000,
-        pager: false,
-        nav: true
-    });
-});
